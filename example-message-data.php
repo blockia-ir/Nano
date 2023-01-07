69e53fccbc2ad7125ed207753d740df6843e750c
Blockia <106010724+blockia-ir@users.noreply.github.com>
2023-01-07 11:29:58 +0330

    Update example-message-data.php

--- example-message-data.php
+++ example-message-data.php
@@ -5,6 +5,7 @@ require 'Nano.php';
 $bot = new Nano('token', 'username');
 
 $bot->photo(function(){ // text | video | document | and ...
+
     $json = json_encode(Nano::message(), JSON_PRETTY_PRINT);
     return Nano::sendMessage("$json");
 });
